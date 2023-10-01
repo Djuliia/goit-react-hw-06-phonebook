b097b55878980dec718b7ad4efd32c9bbaaf8632
Yuliia Chaika <djuliya72@ukr.net>
2023-10-01 22:34:59 +0300

    third commit

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -3,12 +3,14 @@ import { nanoid } from 'nanoid';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const contactsInitialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const contactsInitialState = {
+  items: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+};
 
 const contactSlice = createSlice({
   name: 'contacts',
@@ -16,20 +18,22 @@ const contactSlice = createSlice({
   reducers: {
     addContact: {
       reducer(state, action) {
-        return [action.payload, ...state];
+        state.items.push(action.payload);
       },
       prepare({ name, number }) {
         return {
           payload: {
+            id: nanoid(),
             name,
             number,
-            id: nanoid(),
           },
         };
       },
     },
     deleteContact(state, action) {
-      return state.filter(contact => contact.id !== action.payload);
+      state.items = state.items.filter(
+        contact => contact.id !== action.payload
+      );
     },
   },
 });
